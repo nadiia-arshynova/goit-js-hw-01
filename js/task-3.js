8db4bffcba1f3a55f67515b2a414a911a3d67d90
Nadiia Arshynova <nnarshinova@gmail.com>
2025-08-08 16:43:29 +0300

    my first js homework

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+    const numContent = Number.parseInt(content);
+    const numPadding = Number.parseInt(padding);
+    const numBorder = Number.parseFloat(border);
+    return numContent + numPadding * 2 + numBorder * 2;
+
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
